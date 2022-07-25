add NLBGS solver (need to implement tests), fix issue with custom operation derivative args, fix nlsolver options not being properly read

python_csdl_backend/operations/implicit/newton.py
@@ -5,11 +5,13 @@ from python_csdl_backend.utils.operation_utils import nl_solver_completion_statu
 import scipy.sparse as sp
 
 
-class NewtonSolverLite(ImplicitSolverBase):
+class NewtonLiteSolver(ImplicitSolverBase):
 
     def __init__(self, op, ins, outs):
         super().__init__(op, ins, outs)
-        self.tol = 1e-10
+        # print(op)
+        self.maxiter = op.nonlinear_solver.options['maxiter']
+        self.tol = op.nonlinear_solver.options['atol']
 
     def _solve_implicit(self):
 
@@ -48,6 +50,8 @@ class NewtonSolverLite(ImplicitSolverBase):
             if solved:
                 break
             iter += 1
+            if iter >= self.maxiter:
+                break
 
             # resume Newton iteration:
             # compute jacobian

python_csdl_backend/tests/create_single_test.py
@@ -22,8 +22,10 @@ def run_test_single(model, outs, ins, name, sparsity_case, vals_dict, totals_dic
                              display_scripts=False)
     # sim_lite.eval_instructions.save()
     sim_lite.run()
+    # print(sim_lite['u'])
+    # exit()
     error_dict = sim_lite.check_partials(compact_print=True)
-
+    
     # check values if given
     for key in vals_dict:
         np.testing.assert_almost_equal(