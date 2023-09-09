5923f5613aaeccc8468bfafdd70f1e60432a1d57
msperryucsd <msperry@ucsd.edu>
2023-09-08 19:28:56 -0700

    fix average

--- python_csdl_backend/tests/test_operations/test_avg/test_axiswise_avg.py
+++ python_csdl_backend/tests/test_operations/test_avg/test_axiswise_avg.py
@@ -3,7 +3,7 @@ import csdl
 import numpy as np
 
 
-class AxisaverageSample(csdl.Model):
+class AverageSample(csdl.Model):
 
     def initialize(self):
         self.parameters.declare('scalability_param')
@@ -20,10 +20,47 @@ class AxisaverageSample(csdl.Model):
         # print('Y SHAPE:', y.shape)
 
 
-def test_axisaverage():
+def test_average():
     n = 3
     val = np.arange(n)
     y = np.average(val)
+    # print(y)
+    # exit()
+    vals_dict = {'y': y}
+
+    # total = np.eye(nn)
+    totals_dict = {}
+
+    run_test(
+        AverageSample(scalability_param=n),
+        outs=['y'],
+        ins=['x'],
+        name='test_axisaverage',
+        vals_dict=vals_dict,
+        totals_dict=totals_dict)
+
+
+class AxisaverageSample(csdl.Model):
+
+    def initialize(self):
+        self.parameters.declare('scalability_param')
+
+    def define(self):
+        n = self.parameters['scalability_param']
+
+        # Declare a vector of length 3 as input
+        v1 = self.create_input('v1', val=np.arange(n).reshape((int(n/2), 2)))
+
+        # Output the average of all the elements of the vector v1
+        y = csdl.average(v1, axes=0)
+        self.register_output('y', y)
+        # print('Y SHAPE:', y.shape)
+
+
+def test_axisaverage():
+    n = 30
+    val = np.arange(n).reshape((int(n/2), 2))
+    y = np.average(val, axis=0)
     # print(y)
     # exit()
     vals_dict = {'y': y}
--- python_csdl_backend/tests/test_operations/test_avg/test_multiple_tensor.py
+++ python_csdl_backend/tests/test_operations/test_avg/test_multiple_tensor.py
@@ -27,6 +27,30 @@ class ExampleMultipleTensor(csdl.Model):
                              csdl.average(T1, T2))
 
 
+class ExampleAxisMultipleTensor(csdl.Model):
+    def define(self):
+        n = 3
+        m = 6
+        p = 7
+        q = 10
+
+        # Declare a tensor of shape 3x6x7x10 as input
+        T1 = self.declare_variable(
+            'x1',
+            val=np.arange(n * m * p * q).reshape((n, m, p, q)),
+        )
+
+        # Declare another tensor of shape 3x6x7x10 as input
+        T2 = self.declare_variable(
+            'x2',
+            val=np.arange(n * m * p * q, 2 * n * m * p * q).reshape(
+                (n, m, p, q)),
+        )
+        # Output the elementwise average of tensors T1 and T2
+        self.register_output('y',
+                             csdl.average(T1, T2, axes=3))
+
+
 def test_mult_tensor_average():
     n = 3
     m = 6
@@ -46,6 +70,34 @@ def test_mult_tensor_average():
 
     run_test(
         ExampleMultipleTensor(),
+        outs=['y'],
+        ins=['x1', 'x2'],
+        name='test_mult_tensor_average',
+        vals_dict=vals_dict,
+        totals_dict=totals_dict)
+
+
+def test_mult_tensor_average_axis():
+    n = 3
+    m = 6
+    p = 7
+    q = 10
+
+    T1 = np.arange(n * m * p * q).reshape((n, m, p, q))
+    T2 = np.arange(n * m * p * q, 2 * n * m * p * q).reshape((n, m, p, q))
+
+    y = (T1 + T2) / 2.
+    # y = np.average(T1, T2)
+    y = np.average(y, axis=3)
+    # print(y)
+    # exit()
+    vals_dict = {'y': y}
+
+    # total = np.eye(nn)
+    totals_dict = {}
+
+    run_test(
+        ExampleAxisMultipleTensor(),
         outs=['y'],
         ins=['x1', 'x2'],
         name='test_mult_tensor_average',
